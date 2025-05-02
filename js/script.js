b45712c6574937e94d6e155789cce44a84f5e8e8
DipuBaroi <dipubaroi758@gmail.com>
2025-05-02 19:50:13 +0600

    added an index.html file, an images folder, a js folder. and brad color section done . size section on going.

---
+++ js/script.js
@@ -0,0 +1,39 @@
+const ringButtons = document.querySelectorAll('.ring-button')
+
+for(i = 0; i < ringButtons.length; i++){
+    const ringBtn = ringButtons[i];
+    ringBtn.addEventListener('click', function(event){
+
+        const color = event.target.id.replace('-color' ,'')
+        console.log(color)
+
+    for(j = 0; j < ringButtons.length; j++){
+        ringButtons[j].classList.remove('border-purple-600')
+        ringButtons[j].classList.add('border-gray-300')
+    }
+     event.target.classList.add('border-purple-600');
+     event.target.classList.remove('border-gray-300');
+     
+     const productImage = document.getElementById('product-image')
+    //  productImage.src="../images/teal.png"
+    productImage.src="../images/" + color + ".png"
+    })
+
+    
+    
+}
+
+function selectWristSize(size){
+    const sizes = ["S", "M", "L", "XL"]
+    for(i=0; i<sizes.length; i++){
+        const button = document.getElementById("size-" + sizes[i])
+        // console.log(button)
+        const element = sizes[i];
+        if(size === element){
+            button.classList.add('border-purple-600')
+        }
+        else{
+            button.classList.remove('border-purple-600')
+        }
+    }
+}
